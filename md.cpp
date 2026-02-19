c82fa1d810b5593a1a8117b9852c700b15af27e2
FadelSM <rasyahstore8888@gmail.com>
2026-02-19 10:44:56 +0700

    Update and rename README.md to md.cpp

---
+++ md.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Ini fungsi yang kamu kerjakan (10 poin tadi)
+int maximumDraws(int n) {
+    return n + 1;
+}
+
+// INI YANG HILANG/ERROR. Jangan dihapus!
+int main() {
+    int t;
+    cin >> t; // Membaca jumlah test case
+    while (t--) {
+        int n;
+        cin >> n; // Membaca input n
+        cout << maximumDraws(n) << endl; // Cetak hasil
+    }
+    return 0;
+}
